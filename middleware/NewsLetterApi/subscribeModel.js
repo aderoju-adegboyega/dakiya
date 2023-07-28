2291a946f92359d304a90efc0f6a9773caa660ca
aderoju-adegboyega <50000638+aderoju-adegboyega@users.noreply.github.com>
2023-07-28 00:41:15 -0300

    Delete middleware directory

--- middleware/NewsLetterApi/subscribeModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const subscribeSchema = new mongoose.Schema({
-    email: String,
-});
-
-const Subscribe = mongoose.model('subscribeUsers', subscribeSchema);
-
-module.exports = Subscribe;
